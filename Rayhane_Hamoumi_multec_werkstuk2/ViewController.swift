ecb497cb80540505fd9a0cf834346188703e9de8
muge sonmez <mugesonmez@macbook-air-van-muge.local>
2018-05-13 05:13:06 +0200

    Mapview, Talen kunnen veranderen met buttons en constraints

--- Rayhane_Hamoumi_multec_werkstuk2/ViewController.swift
+++ Rayhane_Hamoumi_multec_werkstuk2/ViewController.swift
@@ -11,46 +11,30 @@ import MapKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet var NaamLanden: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let initialLocation = CLLocation(latitude: 50.841603, longitude: 4.323415)
-        let regionRadius: CLLocationDistance = 1000
-        func centerMapOnLocation(location: CLLocation) {
-            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius, regionRadius)
-            mapView.setRegion(coordinateRegion, animated: true)
-        }
-        centerMapOnLocation(location: initialLocation)
-        mapView.delegate = self
-        
-        let Vilo = ViloStation(title: "ErasmusHogeschool",
-                              locationName: "Campus Kaai",
-                              coordinate: CLLocationCoordinate2D(latitude: 50.841603, longitude: 4.323415))
-        mapView.addAnnotation(Vilo)
+       
     }
-
+    @IBAction func Dutch(_ sender: UIButton) {
+        let Taal = Bundle.main.path(forResource: "nl", ofType: "lproj")
+        let NL = Bundle.init(path: Taal!)! as Bundle
+        NaamLanden.text = NL.localizedString(forKey: "CountryName", value: nil, table: nil)
+    }
+    @IBAction func English(_ sender: UIButton) {
+        let Taal = Bundle.main.path(forResource: "en", ofType: "lproj")
+        let Eng = Bundle.init(path: Taal!)! as Bundle
+        NaamLanden.text = Eng.localizedString(forKey: "CountryName", value: nil, table: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
 
 }
-extension ViewController: MKMapViewDelegate {
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        guard let annotation = annotation as? ViloStation else { return nil }
-        let identifier = "marker"
-        var view: MKMarkerAnnotationView
-        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-            as? MKMarkerAnnotationView {
-            dequeuedView.annotation = annotation
-            view = dequeuedView
-        } else {
-            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            view.canShowCallout = true
-            view.calloutOffset = CGPoint(x: -5, y: 5)
-            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }
-        return view
-    }
-}
+
 
